37ae30da91e3f752a08a27ea7bfd79b2f676b87b
arthas1888 <tavomorales88@gmail.com>
2016-11-18 10:56:27 -0500

    clase 6

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,14 +1,20 @@
 app.controller('BooksController', BooksController);
 app.controller('CustomController', CustomController);
+app.controller('PanelController', PanelController);
+app.controller('ReviewController', ReviewController);
 
 BooksController.$inject = ['$scope'];
 function BooksController($scope) {
     //$scope.book = book;
     $scope.books = books;
     $scope.msg = "Mensaje";
-    $scope.fecha = 1288323623006 ;
+    $scope.fecha = 1288323623006;
 
-    $scope.$watch('msg', function(newValue, oldValue) {
+    $scope.init = function () {
+        $scope.tab = 1;
+    };
+
+    $scope.$watch('msg', function (newValue, oldValue) {
         console.log("entra", newValue, oldValue);
     });
 }
@@ -16,4 +22,32 @@ function BooksController($scope) {
 CustomController.$inject = ['$scope'];
 function CustomController($scope) {
 
+}
+
+PanelController.$inject = ['$scope'];
+function PanelController($scope) {
+    $scope.tab = 1;
+    $scope.selecTab = function (tab) {
+        $scope.tab = tab;
+    };
+    $scope.isSelected = function (tab) {
+        return $scope.tab === tab;
+    };
+
+}
+
+ReviewController.$inject = ['$scope'];
+function ReviewController($scope) {
+    $scope.model = {
+        estrellas: "",
+        cuerpo: "",
+        autor: ""
+    };
+    console.log("review: ", $scope.model );
+
+    $scope.submit = function(){
+        console.log("entra aca", $scope.model);
+        $scope.book.comentarios.push($scope.model);
+        $scope.model = {};
+    };
 }
--- public/javascripts/jsonBooks.js
+++ public/javascripts/jsonBooks.js
@@ -10,49 +10,93 @@
 
 var books = [
     {
-        "id": 122325,
         "year": "1967",
         "name": "Cien años de soledad",
         "autor": "Gabriel Garcia",
-        "descripcion": "Cien años de soledad es una novela del escritor colombiano Gabriel García Márquez, ganador del Premio Nobel de Literatura en 1982",
         "price": 5,
-        "canPurchase": false,
-        "agotado": false,
-    },
-    {
-        "year": "1605",
-        "name": "Don Quijote de la mancha",
-        "autor": "Miguel de Cervantes",
-        "descripcion": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vitae nulla eget ligula ornare luctus vel ac nibh. Nulla a arcu non velit rhoncus imperdiet. Nullam non magna pretium, molestie nibh id, maximus lacus. Sed ut pharetra lectus, at ullamcorper odio. Etiam faucibus, risus mattis varius suscipit, orci nibh elementum velit.",
-        "price": 15,
+        "descripcion": "Es considerada una obra maestra de la literatura hispanoamericana y universal, así como una de las obras más traducidas y leídas en español.",
         "canPurchase": true,
         "agotado": false,
+        "image": "images/Cien-anos-de-soledad.jpg",
+        "comentarios": [
+            {
+                "estrellas": 5,
+                "cuerpo": "Me gusta este libro",
+                "autor": "andres"
+            }
+        ]
     },
     {
-        "year": "1841",
-        "name": "Los crímenes de la calle Morgue",
+        "year": "1873",
+        "name": "La vuelta al mundo en ochenta dias",
+        "autor": "Julio Verne",
+        "price": 5,
+        "descripcion": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris purus nisl, feugiat ac porta quis, maximus vitae odio. Suspendisse eu nisl ipsum. Duis sed ultricies elit. Donec tincidunt semper diam, sed mollis dui posuere eget. Nam vel nisl faucibus, elementum massa a, malesuada ex. Donec interdum feugiat sodales. Vestibulum euismod erat quis urna interdum, in elementum turpis vulputate. Curabitur consectetur mi felis, feugiat eleifend augue porttitor sed. Aliquam scelerisque malesuada libero. Nam finibus eros at vestibulum tincidunt. Fusce fringilla id ligula ut placerat. Etiam nec tellus vel velit vestibulum malesuada nec nec leo. Fusce tristique ultrices laoreet. Nam id viverra eros, nec imperdiet lectus. Sed maximus quam dui, in luctus velit finibus sed. Quisque quis mauris eget libero malesuada vestibulum non gravida ipsum.",
+        "canPurchase": false,
+        "agotado": false,
+        "image": "images/vuelta-al-mundo.jpg",
+        "comentarios": [
+            {
+                "estrellas": 4,
+                "cuerpo": "Un libro para repetir",
+                "autor": "luis"
+            }
+        ]
+    },
+    {
+        "year": "1843",
+        "name": "Corazon delator",
         "autor": "Edgar Allan Poe",
-        "descripcion": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vitae nulla eget ligula ornare luctus vel ac nibh. Nulla a arcu non velit rhoncus imperdiet. Nullam non magna pretium, molestie nibh id, maximus lacus. Sed ut pharetra lectus, at ullamcorper odio. Etiam faucibus, risus mattis varius suscipit, orci nibh elementum velit.",
         "price": 10,
+        "descripcion": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris purus nisl, feugiat ac porta quis, maximus vitae odio. Suspendisse eu nisl ipsum. Duis sed ultricies elit. Donec tincidunt semper diam, sed mollis dui posuere eget. Nam vel nisl faucibus, elementum massa a, malesuada ex. Donec interdum feugiat sodales. Vestibulum euismod erat quis urna interdum, in elementum turpis vulputate. Curabitur consectetur mi felis, feugiat eleifend augue porttitor sed. Aliquam scelerisque malesuada libero. Nam finibus eros at vestibulum tincidunt. Fusce fringilla id ligula ut placerat. Etiam nec tellus vel velit vestibulum malesuada nec nec leo. Fusce tristique ultrices laoreet. Nam id viverra eros, nec imperdiet lectus. Sed maximus quam dui, in luctus velit finibus sed. Quisque quis mauris eget libero malesuada vestibulum non gravida ipsum.",
         "canPurchase": false,
         "agotado": false,
+        "image": "images/corazon-delator.jpg",
+        "comentarios": [
+            {
+                "estrellas": 4,
+                "cuerpo": "Me gusta el autor",
+                "autor": "andrea"
+            }
+        ]
     },
     {
-        "year": "1942",
-        "name": "El extranjero",
-        "autor": "Albert Camus",
-        "descripcion": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vitae nulla eget ligula ornare luctus vel ac nibh. Nulla a arcu non velit rhoncus imperdiet. Nullam non magna pretium, molestie nibh id, maximus lacus. Sed ut pharetra lectus, at ullamcorper odio. Etiam faucibus, risus mattis varius suscipit, orci nibh elementum velit.",
-        "price": 8,
+        "year": "1943",
+        "name": "El principito",
+        "autor": "Antoine de Saint-Exupéry",
+        "price": 15,
+        "descripcion": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris purus nisl, feugiat ac porta quis, maximus vitae odio. Suspendisse eu nisl ipsum. Duis sed ultricies elit. Donec tincidunt semper diam, sed mollis dui posuere eget. Nam vel nisl faucibus, elementum massa a, malesuada ex. Donec interdum feugiat sodales. Vestibulum euismod erat quis urna interdum, in elementum turpis vulputate. Curabitur consectetur mi felis, feugiat eleifend augue porttitor sed. Aliquam scelerisque malesuada libero. Nam finibus eros at vestibulum tincidunt. Fusce fringilla id ligula ut placerat. Etiam nec tellus vel velit vestibulum malesuada nec nec leo. Fusce tristique ultrices laoreet. Nam id viverra eros, nec imperdiet lectus. Sed maximus quam dui, in luctus velit finibus sed. Quisque quis mauris eget libero malesuada vestibulum non gravida ipsum.",
         "canPurchase": true,
         "agotado": false,
+        "image": "images/el-principito.jpg",
+        "comentarios": [
+            {
+                "estrellas": 5,
+                "cuerpo": "Excelente este libro",
+                "autor": "juan"
+            },
+            {
+                "estrellas": 4,
+                "cuerpo": "Buen libro",
+                "autor": "Andres"
+            }
+        ]
     },
     {
-        "year": "1866",
-        "name": "Crimen y Castigo",
-        "autor": "Fiódor Dostoyevski",
-        "descripcion": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vitae nulla eget ligula ornare luctus vel ac nibh. Nulla a arcu non velit rhoncus imperdiet. Nullam non magna pretium, molestie nibh id, maximus lacus. Sed ut pharetra lectus, at ullamcorper odio. Etiam faucibus, risus mattis varius suscipit, orci nibh elementum velit.",
-        "price": 4,
+        "year": "1915",
+        "name": "Metamorfosis",
+        "autor": "Franz Kafka",
+        "price": 8,
+        "descripcion": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris purus nisl, feugiat ac porta quis, maximus vitae odio. Suspendisse eu nisl ipsum. Duis sed ultricies elit. Donec tincidunt semper diam, sed mollis dui posuere eget. Nam vel nisl faucibus, elementum massa a, malesuada ex. Donec interdum feugiat sodales. Vestibulum euismod erat quis urna interdum, in elementum turpis vulputate. Curabitur consectetur mi felis, feugiat eleifend augue porttitor sed. Aliquam scelerisque malesuada libero. Nam finibus eros at vestibulum tincidunt. Fusce fringilla id ligula ut placerat. Etiam nec tellus vel velit vestibulum malesuada nec nec leo. Fusce tristique ultrices laoreet. Nam id viverra eros, nec imperdiet lectus. Sed maximus quam dui, in luctus velit finibus sed. Quisque quis mauris eget libero malesuada vestibulum non gravida ipsum.",
         "canPurchase": true,
         "agotado": true,
+        "image": "images/metamorfosis.jpg",
+        "comentarios": [
+            {
+                "estrellas": 5,
+                "cuerpo": "Me gusta este libro",
+                "autor": "andres"
+            }
+        ]
     }
 ];
